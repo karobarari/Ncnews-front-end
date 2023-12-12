a9aa3e9b20647d1e49a4faaa5d1474e2d7ec383e
caro <karobarari@gmail.com>
2023-12-12 15:52:45 +0000

    add View an individual article

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header";
 import DisplaySection from "./components/DisplaySection";
+import ArticleCard from "./components/ArticleCard";
+
 
 function App() {
+  
   return (
     <>
       <Header />
-      <DisplaySection />
+      <Routes>
+        <Route path="/" element={<DisplaySection />} />
+        <Route path="/ArticleCard/:article_id" element={<ArticleCard />} />
+      </Routes>
     </>
   );
 }
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Articles = ({ articles }) => {
+  const [article, setArticle] = useState(0);
+  const navigate = useNavigate();
 
-    const handleClick =()=>{
+  const handleClick = (event, articleId) => {
+    event.preventDefault();
+    setArticle(articleId);
+    navigate(`/ArticleCard/${articleId}`);
+  };
 
-    }
   return (
     <ul>
-      {articles.map((article) => {
-        return (
-          <li key={article.article_id} onClick={handleClick}>
-            <h2>{article.title}</h2>
-            <img src={article.article_img_url} alt="" />
-            <h3>author: {article.author}</h3>
-            <p>Comments: {article.comment_count}</p>
-            <p>votes: {article.votes}</p>
-          </li>
-        );
-      })}
+      {articles.map((article) => (
+        <li
+          value={article.article_id}
+          key={article.article_id}
+          onClick={(event) => handleClick(event, article.article_id)}
+        >
+          <h2>{article.title}</h2>
+          <img src={article.article_img_url} alt="" />
+          <h3>author: {article.author}</h3>
+          <p>Comments: {article.comment_count}</p>
+          <p>votes: {article.votes}</p>
+        </li>
+      ))}
     </ul>
   );
 };
+
 export default Articles;
