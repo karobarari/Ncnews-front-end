6587a878bb08720c44425882266ab7e7b7c296df
caro <karobarari@gmail.com>
2023-12-17 13:35:40 +0000

    debugging

--- src/components/Home/queryAxios.js
+++ src/components/Home/queryAxios.js
@@ -1,16 +1,29 @@
 import axios from "axios";
 
-export const getQueries = (chosenTopic, order, sortby) => {
-  return axios
-    .get(
-      `https://ncnews-yzwd.onrender.com/api/articles?topic=${chosenTopic}&sort_by=${sortby}&order=${order}`
-    )
-    .then((res) => {
-      return res.data.articles;
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error.message);
-    });
+const instance = axios.create({
+  baseURL: "https://ncnews-yzwd.onrender.com/api",
+});
+
+export const getQueries = (chosenTopic, order = "ASC", sortby) => {
+  if (chosenTopic || order || sortby) {
+    return instance
+      .get(`/articles?topic=${chosenTopic}&sort_by=${sortby}&order=${order}`)
+      .then((res) => {
+        return res.data.articles;
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error.message);
+      });
+  } else {
+    return instance
+      .get(`/articles`)
+      .then((res) => {
+        return res.data.articles;
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error.message);
+      });
+  }
 };
 export const getAlltopics = () => {
   return axios
